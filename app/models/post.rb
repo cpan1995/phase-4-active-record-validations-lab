e7597dc65262680b5a52468a7e91251824e6df28
cpan1995 <cpan1995@gmail.com>
2021-09-01 14:13:31 -0400

    commit tree f325891e29936983802116038222b7412f7d298c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :not_click_baity_enough
+
+    def not_click_baity_enough
+        unless self.title =~ /(Won't Believe|Secret|Top \d*|Guess)/i
+            errors.add(:title, "This isn't click-baity enough")
+        end
+    end
 end
